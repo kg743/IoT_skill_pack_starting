24618ea8892fa47d963bd980e449f81f9be3f830
kg743 <koronaeden@gmail.com>
2019-10-14 08:07:44 +0100

    First commit

---
+++ IoT.py
@@ -0,0 +1,16 @@
+# -*- Coding: utf-8 -*-
+import os
+import subprocess
+import datetime
+from time import sleep
+ 
+def main():
+  dt_now = datetime.datetime.now()
+  print(dt_now.strftime('%Y/%m/%d %H:%M:%S') + "Hello World")
+  print(os.environ.get('LANG'))
+  #print(os.environ.get('RemoteENV'))
+ 
+if __name__ == '__main__':
+  while True:
+    main()
+    sleep(5)
